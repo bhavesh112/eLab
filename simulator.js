bc42fa0cb27cbb23919a99bce562570da52a3278
bhavesh wadhwani <bhaveshwadhwani21@gmail.com>
2020-06-03 14:46:32 +0530

    Algorithm Of Simulator Done

---
+++ simulator.js
@@ -0,0 +1,86 @@
+class Experiment
+{  
+   //  All the datasets are declared.
+   Reagent={
+       MOLISCH : 1 ,
+       FEHLING : 2 ,
+       BENEDICT :3 ,
+       TOLLEN :4,
+       IODINE :5,
+       COPPERSULPHATE : 6,
+       NITRICACID :7
+   };
+  
+
+    //Assigns a random value to sample.
+    constructor()
+    {
+       this.sample = Math.floor(Math.random()*2); //0 for fats and 1 for carbohydrates
+   }
+   
+   //Methods of Experiment
+
+   Add(Reagent)
+   {
+       switch (this.sample)
+       {
+           case 0: switch(Reagent)
+                   {   
+                       case this.Reagent.MOLISCH        :  this.test="Molisch's Test";
+                                                           this.indication= "VioletRing";
+                                                           break;
+                       case this.Reagent.FEHLING        :  this.test="Fehling's Test";
+                                                           this.indication= "RedPrecipitate";
+                                                           break;
+                       case this.Reagent.BENEDICT       :  this.test="Benedict's Test";
+                                                           this.indication= "RedPrecipitate";
+                                                           break;
+                       case this.Reagent.TOLLEN         :  this.test="Tollen's Test";
+                                                           this.indication= "SilverMirror";
+                                                           break;
+                       case this.Reagent.IODINE         :  this.test="Iodine Test"
+                                                           this.indication= "BlueColorSoln";
+                                                           break;
+                       case this.Reagent.COPPERSULPHATE :  this.test="Biuret Test";
+                                                           this.indication="NoEffect";
+                                                           break;
+                       case this.Reagent.NITRICACID     :  this.test="Xanthoproteic Test";
+                                                           this.indication="NoEffect";
+                                                           break;
+
+                   }     
+                   break; 
+           case 1:  switch(Reagent)
+                   {
+                       case this.Reagent.MOLISCH        :  this.test ="Molisch's Test";
+                                                           this.indication= "NoEffect";
+                                                           break;
+                       case this.Reagent.FEHLING        :  this.test ="Fehling's Test";
+                                                           this.indication ="NoEffect";
+                                                           break;
+                       case this.Reagent.BENEDICT       :  this.test ="Benedict's Test";
+                                                           this.indication ="NoEffect";
+                                                           break;
+                       case this.Reagent.TOLLEN         :  this.test="Tollen's Test";
+                                                           this.indication="NoEffect";
+                                                           break;
+                       case this.Reagent.IODINE         :  this.test ="Iodine Test";
+                                                           this.indication="NoEffect";
+                                                           break;
+                       case this.Reagent.COPPERSULPHATE :  this.test="Biuret Test";
+                                                           this.indication="VioletColour";
+                                                           break;
+                       case this.Reagent.NITRICACID     :  this.test="Xanthoproteic Test";
+                                                           this.indication="YellowColour";
+                                                           break;
+                   }
+                   break;
+       }
+   }
+       
+}
+
+myExperiment = new Experiment();
+myExperiment.Add(5);
+console.log(myExperiment.sample);
+console.log(myExperiment.indication);
